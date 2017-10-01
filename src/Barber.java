b690f794d6a6c4637f0ac9d9ab63900bf4ef0f75
gya12 <gya12@my.fsu.edu>
2017-10-01 18:40:46 -0400

    Initial commit

---
+++ src/Barber.java
@@ -0,0 +1,26 @@
+public class Barber {
+    private String name;
+    private int total;
+
+    public Barber(String name, int total) {
+
+        this.name = name;
+        this.total = total;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
---
+++ src/Driver.java
@@ -0,0 +1,101 @@
+import java.util.Random;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Barber[] barbers = new Barber[]{
+                new Barber("A", 0),
+                new Barber("B", 0),
+                new Barber("C", 0),
+                new Barber("D", 0),
+                new Barber("E", 0)
+        };
+
+
+        Barber[] b1 = new Barber[]{
+                new Barber("A", 100),
+                new Barber("B", 100),
+                new Barber("C", 90),
+                new Barber("D", 90),
+                new Barber("E", 80)
+        };
+
+        Barber[] b2 = new Barber[]{
+                new Barber("E", 190),
+                new Barber("A", 190),
+                new Barber("B", 200),
+                new Barber("C", 200),
+                new Barber("D", 180)
+        };
+
+
+        printBarbers(b1);
+        updateOrder(b1);
+        printBarbers(b1);
+
+        //printBarbers(b2);
+        //updateOrder(b2);
+        //printBarbers(b2);
+
+        //swapAndPush test
+        //printBarbers(b1);
+        //swapAndPush(b1, 1, 4);
+        //printBarbers(b1);
+        //swapAndPush(b1, 0,1);
+        //printBarbers(b1);
+
+        //printBarbers(barbers);
+        //addMonies(barbers);
+        //printBarbers(barbers);
+/*
+        //main loop
+        //complete rounds for now
+        for (int i = 0; i < 1; i++) {
+            addMonies(barbers);
+            //sort(barbers);
+
+        }
+*/
+
+    }
+
+    public static void updateOrder(Barber[] barbers) {
+        for(int i  = 0; i < barbers.length; i ++) {
+            fairSort(barbers, i);
+        }
+    }
+
+    public static void fairSort(Barber[] barbers, int index) {
+        System.out.println("fairSort: " + index);
+        Barber indexBarber = barbers[index];
+        for(int i = index; i > 0; i--) {
+            if(barbers[i - 1].getTotal() - indexBarber.getTotal() >= 20) {
+                swapAndPush(barbers, i - 1, i);
+            }
+        }
+    }
+    public static void swapAndPush(Barber[] barbers, int targetIndex, int sourceIndex) {
+        Barber sourceBarber = barbers[sourceIndex];
+        for (int j = sourceIndex; j > targetIndex; j--) {
+            barbers[j] = barbers[j - 1];
+        }
+        barbers[targetIndex] = sourceBarber;
+    }
+
+    public static void addMonies(Barber[] barbers) {
+        Random rand = new Random();
+        for(Barber barber: barbers) {
+            int n = (rand.nextInt(4) + 1) * 10;
+            barber.setTotal(barber.getTotal() + n);
+        }
+    }
+
+    public static void printBarbers(Barber[] barbers) {
+        System.out.println("******");
+        for(Barber barber: barbers) {
+            System.out.println(barber.getName() + ": " + barber.getTotal());
+        }
+        System.out.println("***");
+    }
+}
