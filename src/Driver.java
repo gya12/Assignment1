d08b9a75a9ce6cbf8d3bd94a957e30c0c6588ff5
gya12 <gya12@my.fsu.edu>
2017-10-31 20:05:58 -0400

    finished

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Driver {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         Barber[] barbers = new Barber[]{
                 new Barber("A", 0),
@@ -12,56 +12,50 @@ public class Driver {
                 new Barber("E", 0)
         };
 
+        int numCustomers = 17;
 
-        Barber[] b1 = new Barber[]{
-                new Barber("A", 100),
-                new Barber("B", 100),
-                new Barber("C", 90),
-                new Barber("D", 90),
-                new Barber("E", 80)
-        };
-
-        Barber[] b2 = new Barber[]{
-                new Barber("E", 190),
-                new Barber("A", 190),
-                new Barber("B", 200),
-                new Barber("C", 200),
-                new Barber("D", 180)
-        };
-
-
-        printBarbers(b1);
-        updateOrder(b1);
-        printBarbers(b1);
-        System.out.println("*****************");
-        printBarbers(b2);
-        updateOrder(b2);
-        printBarbers(b2);
-
-        //printBarbers(b2);
-        //updateOrder(b2);
-        //printBarbers(b2);
-
-        //swapAndPush test
-        //printBarbers(b1);
-        //swapAndPush(b1, 1, 4);
-        //printBarbers(b1);
-        //swapAndPush(b1, 0,1);
-        //printBarbers(b1);
-
-        //printBarbers(barbers);
-        //addMonies(barbers);
-        //printBarbers(barbers);
-/*
-        //main loop
-        //complete rounds for now
-        for (int i = 0; i < 1; i++) {
-            addMonies(barbers);
-            //sort(barbers);
-
+        try {
+            numCustomers = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Error: Could not parse number of customers argument, using default value of 17");
         }
-*/
 
+        int numCompleteRounds = numCustomers / 5;
+        int numIncompleteRound = numCustomers % 5;
+
+        System.out.println("Number of customers: " + numCustomers);
+        System.out.println("Initial order and totals: ");
+        printBarbers(barbers);
+
+        //main loop
+        //complete rounds
+        int i;
+        for (i = 0; i < numCompleteRounds; i++) {
+            System.out.println("*********");
+            System.out.println("Round " + (i + 1) + ":");
+            addMonies(barbers);
+            System.out.println("New totals:");
+            printBarbers(barbers);
+            updateOrder(barbers);
+            System.out.println("New order:");
+            printBarbers(barbers);
+        }
+
+        //remaining customers
+        if (numIncompleteRound > 0) {
+            System.out.println("*********");
+            System.out.println("Round " + (i + 1) + ":");
+            addMonies(barbers, numIncompleteRound);
+            System.out.println("New totals:");
+            printBarbers(barbers);
+            updateOrder(barbers);
+            System.out.println("New order:");
+            printBarbers(barbers);
+        }
+
+        System.out.println("*********");
+        System.out.println("Final order and totals: ");
+        printBarbers(barbers);
     }
 
     public static void updateOrder(Barber[] barbers) {
@@ -75,12 +69,7 @@ public class Driver {
         for(int i = index; i > 0; i--) {
             if(barbers[i - 1].getTotal() - indexBarber.getTotal() >= 20) {
                 swapAndPush(barbers, i - 1, index);
-
-                //indexBarber = barbers[index];
-                //keep the indexBarber
-                //update index
                 index = i - 1;
-
             }
         }
     }
@@ -100,11 +89,17 @@ public class Driver {
         }
     }
 
+    public static void addMonies(Barber[] barbers, int remainingCustomers) {
+        Random rand = new Random();
+        for (int i = 0; i < remainingCustomers; i++) {
+            int n = (rand.nextInt(4) + 1) * 10;
+            barbers[i].setTotal(barbers[i].getTotal() + n);
+        }
+    }
+
     public static void printBarbers(Barber[] barbers) {
-        System.out.println("******");
         for(Barber barber: barbers) {
             System.out.println(barber.getName() + ": " + barber.getTotal());
         }
-        System.out.println("***");
     }
 }
